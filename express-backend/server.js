832b944deed30aaa05e8b7b6272e31267468e407
JaimeLucero <jaimeemanuellucero@gmail.com>
2025-07-10 08:23:57 +0800

    mount routes

--- express-backend/server.js
+++ express-backend/server.js
@@ -6,6 +6,14 @@ const port = 8000; // Backend runs on port 5000
 // Routes Mounting
 const roleRoutes = require('./routes/roles_routes');
 const userRoutes = require('./routes/users_routes');
+const approvalRoutes = require('./routes/approvals_routes');
+const borrowRequestsRoutes = require('./routes/borrow_requests_routes');
+const borrowItemsRoutes = require('./routes/borrow_items_routes');
+const toolRoutes = require('./routes/tools_routes');
+const categoryRoutes = require('./routes/categories_routes');
+const statusRoutes = require('./routes/statuses_routes');
+const groupRoutes = require('./routes/groups_routes');
+const releaseRoutes = require('./routes/release_routes');
 
 
 const cors = require('cors');
@@ -22,6 +30,14 @@ app.get('/', (req, res) => {
 // Mount routes
 app.use('/api/roles', roleRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/approvals', approvalRoutes);
+app.use('/api/borrow-requests', borrowRequestsRoutes);
+app.use('/api/borrow-items', borrowItemsRoutes);
+app.use('/api/tools', toolRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/statuses', statusRoutes);
+app.use('/api/groups', groupRoutes);
+app.use('/api/releases', releaseRoutes);
 
 
 // Start the server
